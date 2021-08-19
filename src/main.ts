34eb3e03c2e1e222b8f930c069a492d319f3d6cc
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-08-19 10:03:45 -0300

    V1 (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,112 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Octokit } from '@octokit/action';
+import minimatch from 'minimatch';
+
+const { GITHUB_REPOSITORY = '', GITHUB_REF = '' } = process.env;
+
+const [owner, repo] = GITHUB_REPOSITORY.split('/');
+const issue_number = GITHUB_REF.split('/')[2];
+const configPath = core.getInput('configuration-path');
+
+const octokit = new Octokit();
+
+async function getJSON(repoPath: string) {
+  const response = await octokit.repos.getContent({
+    owner,
+    repo,
+    path: repoPath,
+    ref: github.context.sha,
+  });
+
+  return JSON.parse(
+    Buffer.from(
+      (response.data as any).content,
+      (response.data as any).encoding
+    ).toString()
+  );
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const files = await octokit.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: parseInt(issue_number),
+    });
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const { added } = await getJSON(configPath);
 
-    core.setOutput('time', new Date().toTimeString())
+    const invalidFiles = files.data
+      .filter((file: { status: string; filename: any }) => {
+        if (file.status === 'added') {
+          return false;
+        }
+        if (
+          Array.isArray(added.ignore) &&
+          added.ignore.some((ignore: string) =>
+            minimatch(file.filename, ignore as string)
+          )
+        ) {
+          return false;
+        }
+
+        return (
+          minimatch(file.filename, '**/*.js') ||
+          minimatch(file.filename, '**/*.jsx')
+        );
+      })
+      .map((file) => file.filename);
+    if (invalidFiles.length > 0) {
+      const js = invalidFiles.filter((name) => name.endsWith('.js'));
+      const jsx = invalidFiles.filter((name) => name.endsWith('.jsx'));
+      const jsMessage = js.length && `${js.length} .js`;
+      const jsxMessage = jsx.length && `${jsx.length} .jsx`;
+
+      const message = `You have added ${[jsMessage, jsxMessage]
+        .filter(Boolean)
+        .join(' and ')} files, please convert to ts(x). \n ${invalidFiles.join(
+        '\n'
+      )}`;
+
+      await octokit.pulls.createReview({
+        owner,
+        event: 'REQUEST_CHANGES',
+        repo,
+        pull_number: parseInt(issue_number),
+        body: message,
+      });
+      throw new Error(message);
+    }
+
+    const reviews = await octokit.pulls.listReviews({
+      owner,
+      repo,
+      pull_number: parseInt(issue_number),
+    });
+    const dismiss = reviews.data.filter((review) => {
+      return (
+        review.state === 'CHANGES_REQUESTED' &&
+        review.user?.login === 'github-actions[bot]' &&
+        review.body.includes('You have added') &&
+        review.body.includes('files, please convert to ts(x).')
+      );
+    });
+
+    dismiss.map((d) => {
+      return octokit.pulls.dismissReview({
+        owner,
+        repo,
+        pull_number: parseInt(issue_number),
+        review_id: d.id,
+        message: 'js files removed',
+      });
+    });
+
+    core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed((error as any).message);
   }
 }
 
-run()
+run();
